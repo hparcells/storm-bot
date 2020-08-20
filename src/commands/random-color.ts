8a2987a1d4d52e63f2e95ddc13d527a1151eaf76
Hunter Parcells <hparcells@icloud.com>
2020-08-20 16:56:56 -0400

    Random Color

---
+++ src/commands/random-color.ts
@@ -0,0 +1,30 @@
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+
+class RandomColorCommand extends Command {
+  constructor() {
+    super('randomcolor', {
+      aliases: ['randomcolor']
+    });
+  }
+
+  async exec(message: Message) {
+    const color = Math.floor(Math.random() * 16777215).toString(16);
+
+    message.channel.send({
+      embed: {
+        title: 'Random Color',
+        description: `#${color}`,
+        color: `#${color}`,
+        thumbnail: {
+          url: `https://singlecolorimage.com/get/${color}/500x500`
+        },
+        footer: {
+          text: 'Image Using singlecolorimage.com'
+        }
+      }
+    });
+  }
+}
+
+export default RandomColorCommand;
